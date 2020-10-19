8146aea57de7943d92b612cb5ee45fe8596405d9
Nicolas Queijo <queijonicolas@gmail.com>
2020-10-19 00:22:59 -0400

    Add Google Maps directions intent.

--- app/src/main/java/com/nicoqueijo/celero/view/CustomerDialogFragment.kt
+++ app/src/main/java/com/nicoqueijo/celero/view/CustomerDialogFragment.kt
@@ -1,5 +1,7 @@
 package com.nicoqueijo.celero.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +13,7 @@ import com.nicoqueijo.celero.R
 import com.nicoqueijo.celero.adapter.ProblemPictureAdapter
 import com.nicoqueijo.celero.databinding.DialogCustomerBinding
 import com.nicoqueijo.celero.model.Customer
+
 
 class CustomerDialogFragment(private val customer: Customer) : DialogFragment() {
 
@@ -25,12 +28,35 @@ class CustomerDialogFragment(private val customer: Customer) : DialogFragment() 
             inflater, R.layout.dialog_customer, container, false
         )
         binding.customer = customer
+        initRecyclerView(binding)
+        initOnClickListeners(binding)
+        return binding.root
+    }
+
+    private fun initRecyclerView(binding: DialogCustomerBinding) {
         adapter = ProblemPictureAdapter(customer.problemPictures)
         binding.problemPictures.adapter = adapter
         binding.problemPictures.setHasFixedSize(true)
         Glide.with(binding.root)
             .load(customer.profilePicture.large)
             .into(binding.profilePicture)
-        return binding.root
+    }
+
+    private fun initOnClickListeners(binding: DialogCustomerBinding) {
+        binding.addressLine1Value.setOnClickListener {
+            launchDirectionsIntent()
+        }
+        binding.addressLine2Value.setOnClickListener {
+            launchDirectionsIntent()
+        }
+    }
+
+    private fun launchDirectionsIntent() {
+        val gmmIntentUri =
+            Uri.parse("google.navigation:q=${customer.location.coordinate.latitude}," +
+                    "${customer.location.coordinate.longitude}&mode=d")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
     }
 }
